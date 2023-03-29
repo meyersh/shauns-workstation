bcd97b9d23935e75d0e95f3e84fa964c48c05f7d
Shaun Meyer <meyersh@morningside.edu>
2023-03-29 08:13:30 -0500

    Add basic vault token checking

--- scripts/vault-sign-ssh-keys.sh
+++ scripts/vault-sign-ssh-keys.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -o allexport; source ~/.env; set +o allexport
 
+# Check Vault login; resolves an issue where I had an expired
+# VAULT_TOKEN env variable. This will check the on-disk (when VAULT_TOKEN) 
+# is empty but also check the variable if it is not empty.
+# 
+# The script will still fail, but you have a prayer because one of the error
+# messages is "The VAULT_TOKEN environmental variable is set!"
+#
+if ! [ -e ~/.vault-token ] || ! vault token lookup "${VAULT_TOKEN}"; then
+    echo "Vault doesn't appear to be logged in, assuming username=${USER}..."
+    vault login -address=https://vault.morningside.edu:8200 -method=ldap username="${USER}"
+fi
+
 for public_key in $HOME/.ssh/id*.pub; do
     if [[ ${public_key} == *"-cert.pub" ]]; then 
         continue; 
